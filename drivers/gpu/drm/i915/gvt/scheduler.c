teslamotors/linux

drm/i915/gvt: inject error interrupt to DomU when GPU hang
When GVT finds a request from DomU causes GPU hang, it will trigger
an error interrupt to guest, so that DomU can trigger a virtual GPU
reset.

Change-Id: I49d9339e99ebfdbe9b158ba311655ab356562bae
Signed-off-by: Min He <min.he@intel.com>
Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>
Reviewed-on: https://android.intel.com/613380
Reviewed-by: Jiang, Fei <fei.jiang@intel.com>
Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
Tested-by: Dong, Eddie <eddie.dong@intel.com>

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -292,6 +292,7 @@ static int dispatch_workload(struct intel_vgpu_workload *workload)
 		goto out;
 	}
 
+	workload->guilty_count = atomic_read(&workload->req->ctx->guilty_count);
 out:
 	if (ret)
 		workload->status = ret;
@@ -493,6 +494,9 @@ static void complete_current_workload(struct intel_gvt *gvt, int ring_id)
 
 	mutex_lock(&gvt->lock);
 	list_del_init(&workload->list);
+	if (workload->status == -EIO)
+		intel_vgpu_reset_execlist(vgpu, 1 << ring_id);
+
 	workload->complete(workload);
 
 	atomic_dec(&vgpu->running_workload_num);
@@ -503,6 +507,18 @@ static void complete_current_workload(struct intel_gvt *gvt, int ring_id)
 
 	mutex_unlock(&gvt->lock);
 	mutex_unlock(&gvt->sched_lock);
+}
+
+static void inject_error_cs_irq(struct intel_vgpu *vgpu, int ring_id)
+{
+	enum intel_gvt_event_type events[] = {
+		RCS_CMD_STREAMER_ERR,
+		BCS_CMD_STREAMER_ERR,
+		VCS_CMD_STREAMER_ERR,
+		VCS2_CMD_STREAMER_ERR,
+		VECS_CMD_STREAMER_ERR,
+	};
+	intel_vgpu_trigger_virtual_event(vgpu, events[ring_id]);
 }
 
 struct workload_thread_param {
@@ -576,6 +592,17 @@ static int workload_thread(void *priv)
 				workload, lret);
 		if (lret >= 0 && workload->status == -EINPROGRESS)
 			workload->status = 0;
+
+		/*
+		 * increased guilty_count means that this request triggerred
+		 * a GPU reset, so we need to notify the guest about the
+		 * hang.
+		 */
+		if (workload->guilty_count <
+				atomic_read(&workload->req->ctx->guilty_count)) {
+			workload->status = -EIO;
+			inject_error_cs_irq(workload->vgpu, ring_id);
+		}
 
 complete:
 		gvt_dbg_sched("will complete workload %p, status: %d\n",

minhe1/acrn-kernel

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -730,6 +730,7 @@ static int dispatch_workload(struct intel_vgpu_workload *workload)
 		goto out;
 	}
 
+	workload->guilty_count = atomic_read(&workload->req->ctx->guilty_count);
 out:
 	if (ret)
 		workload->status = ret;
@@ -950,6 +951,9 @@ static void complete_current_workload(struct intel_gvt *gvt, int ring_id)
 
 	list_del_init(&workload->list);
 
+	if (workload->status == -EIO)
+		intel_vgpu_reset_submission(vgpu, 1 << ring_id);
+
 	if (!workload->status) {
 		release_shadow_batch_buffer(workload);
 		if(gvt_shadow_wa_ctx)
@@ -983,6 +987,18 @@ static void complete_current_workload(struct intel_gvt *gvt, int ring_id)
 
 	mutex_unlock(&gvt->sched_lock);
 	mutex_unlock(&vgpu->vgpu_lock);
+}
+
+static void inject_error_cs_irq(struct intel_vgpu *vgpu, int ring_id)
+{
+	enum intel_gvt_event_type events[] = {
+		RCS_CMD_STREAMER_ERR,
+		BCS_CMD_STREAMER_ERR,
+		VCS_CMD_STREAMER_ERR,
+		VCS2_CMD_STREAMER_ERR,
+		VECS_CMD_STREAMER_ERR,
+	};
+	intel_vgpu_trigger_virtual_event(vgpu, events[ring_id]);
 }
 
 struct workload_thread_param {
@@ -1053,6 +1069,17 @@ static int workload_thread(void *priv)
 				workload, lret);
 		if (lret >= 0 && workload->status == -EINPROGRESS)
 			workload->status = 0;
+
+		/*
+		 * increased guilty_count means that this request triggerred
+		 * a GPU reset, so we need to notify the guest about the
+		 * hang.
+		 */
+		if (workload->guilty_count <
+				atomic_read(&workload->req->ctx->guilty_count)) {
+			workload->status = -EIO;
+			inject_error_cs_irq(workload->vgpu, ring_id);
+		}
 
 complete:
 		gvt_dbg_sched("will complete workload %p, status: %d\n",